ccb96ce4b90e699640e6bdb0c73ddc13ac43813b
ShahirahMokthar <113413324+ShahirahMokthar@users.noreply.github.com>
2024-04-23 06:02:59 +0000

    Root Component

---
+++ videoapp/src/root/rootcomponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyRootComponent() {
+    return ( 
+        <div>
+            <h1>Root Component</h1>
+        </div>
+     );
+}
+
+export default MyRootComponent;
